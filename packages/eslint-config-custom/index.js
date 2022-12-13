d1773ac9dbb0c7408300f25f25b3b194fcb27473
JengYoung <young_pallete@naver.com>
2022-12-13 14:42:37 +0900

    fix(eslint): fixed cannot find module next/babel, add typescript-eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,15 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+  'extends': ['next', 'turbo', 'prettier'],
+  'rules': {
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
   },
+  'overrides': [
+    {
+      'files': '**/*.+(ts|tsx)',
+      'parser': '@typescript-eslint/parser',
+      'plugins': ['@typescript-eslint'],
+      'extends': ['plugin:@typescript-eslint/recommended'],
+    },
+  ],
 };
