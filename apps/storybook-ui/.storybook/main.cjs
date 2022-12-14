d4b7653e470bb0529b4c1a6edb6593f6f9e3d955
JengYoung <young_pallete@naver.com>
2022-12-15 02:29:24 +0900

    chore(storybook): added ChakraProvider by using Decorator
    - 6시간의 엄청난 삽질 끝에 결국 해결했다.
    - 아직 `ChakraProvider`을 애드온으로 추가하기에는 `Vite` 환경에서 매우 불안정한 모습이 보였다.
    - 따라서 직접 의존성 패키지를 모두 설치 후 데코레이터에서 만듦으로써 해결했다.
    - https://github.com/chakra-ui/chakra-ui/issues/6433

--- apps/storybook-ui/.storybook/main.cjs
+++ apps/storybook-ui/.storybook/main.cjs
@@ -1,3 +1,5 @@
+const { mergeConfig } = require('vite');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,13 +8,31 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    // "@chakra-ui/storybook-addon",
   ],
   "framework": "@storybook/react",
+  // NOTE: https://github.com/chakra-ui/chakra-ui/issues/6433
   "core": {
     "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true
-  }
+    "storyStoreV7": true,
+    "emotionAlias": false,
+  },
+  
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      module: {
+        rules: [
+          {
+            test: /\.mjs$/,
+            include: /node_modules/,
+            type: 'javascript/auto',
+          }
+        ]
+      }
+    });
+  },
 }
