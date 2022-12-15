7ba16d549ef76be2a9c3a16135cb982b9602aeaf
JengYoung <young_pallete@naver.com>
2022-12-15 23:27:00 +0900

    feat(css-style): added global style(reset.css)

--- packages/ui/styles/CustomThemeProvider.tsx
+++ packages/ui/styles/CustomThemeProvider.tsx
@@ -2,11 +2,18 @@ import React from "react";
 
 import { ThemeProvider } from "@emotion/react";
 import { globalTheme } from "./globalTheme";
+import { Global } from "@emotion/react";
+import { globalStyle } from "./globalStyle";
 
 export function CustomThemeProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ThemeProvider theme={globalTheme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={globalTheme}>
+      <Global styles={globalStyle} />
+      {children}
+    </ThemeProvider>
+  );
 }
