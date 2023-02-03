4695ef77974b8776fedf770bb3209e3286f20e17
JengYoung <young_pallete@naver.com>
2023-02-03 14:19:17 +0900

    fix(component): fixed page resize not working
    - f47f4490에서 바꾼 useEffect의 deps의 경우 함수가 초기화되면 초깃값으로 리렌더링을 하게 된다.
    - 이때, onChange를 하는 순간 함수형 컴포넌트가 렌더링되고, 이에 따라 함수가 다시 렌더링되므로 무한 루프가 발생했다.
    - 이를 해결하기 위해 deps를 초기에만 작동하도록 빈배열로 설정했다.
    
    Refs: f47f4490

--- apps/web/pages/template/create/index.tsx
+++ apps/web/pages/template/create/index.tsx
@@ -9,11 +9,16 @@ import { useResizablePageAtom } from '@hooks/index';
 export default function TemplateCreatePage() {
   const { pageState, setPageWidth, setPageHeight, setPageScale } = useResizablePageAtom();
 
+  /**
+   * @todo
+   * TODO: 기본적으로 설정할 너비와 높이를 커스터마이징한다면 이를 수정하면 된다.
+   */
   useEffect(() => {
     setPageWidth({ width: '500px' });
     setPageHeight({ height: '1000px' });
     setPageScale({ scale: '1' });
-  }, [setPageHeight, setPageWidth, setPageScale]);
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  }, []);
 
   return <ResizablePage width={pageState.width} height={pageState.height} />;
 }
