ee2336fa35e96348f55a1eaea8088d49fce384c0
JengYoung <young_pallete@naver.com>
2023-02-04 23:16:52 +0900

    refactor(component): refactor Box to deliver forwardRef

--- packages/ui/box/Default.tsx
+++ packages/ui/box/Default.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 import { Box, BoxProps } from '@chakra-ui/react';
 
-export function DefaultBox({ children, ...props }: BoxProps) {
-  return <Box {...props}>{children}</Box>;
-}
+export const DefaultBox = React.forwardRef(
+  ({ children, ...props }: BoxProps, ref: React.LegacyRef<HTMLDivElement>) => {
+    return (
+      <Box ref={ref} {...props}>
+        {children}
+      </Box>
+    );
+  }
+);
+
+DefaultBox.displayName = 'DefaultBox';
